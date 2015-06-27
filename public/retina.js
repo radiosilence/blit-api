329790ffffe40c01dd976bad8cb824e283cc01bb
James Cleveland <jc@blit.cc>
2015-06-27 03:09:43 +0100

    put height/width on image before load

--- public/retina.js
+++ public/retina.js
@@ -1,8 +1,3 @@
 var img = document.getElementById('img');
-img.style.opacity = 0;
-img.addEventListener('load', function() {
-  window.requestAnimationFrame(function() {
-    img.width = img.width / window.devicePixelRatio;
-    img.style.opacity = 1;
-  });
-});
+img.width = img.width / window.devicePixelRatio;
+img.height = img.height / window.devicePixelRatio;
